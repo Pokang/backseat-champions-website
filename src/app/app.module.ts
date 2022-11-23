0fe52db3ffe6d4b81063af95f93bc5a78831566d
Raphael Boishus <31265372+Pokang@users.noreply.github.com>
2022-11-23 15:02:14 +0100

    normalize css, main & playbutton comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainbannerComponent } from './mainbanner/mainbanner.component';
+import { PlaybuttonComponent } from './playbutton/playbutton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainbannerComponent,
+    PlaybuttonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mainbanner/mainbanner.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainbannerComponent } from './mainbanner.component';
+
+describe('MainbannerComponent', () => {
+  let component: MainbannerComponent;
+  let fixture: ComponentFixture<MainbannerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MainbannerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MainbannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
